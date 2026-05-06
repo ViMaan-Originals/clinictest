85d7d648ca0fc68b94383ae547cbafd007159e50
vivek <vivekpooja541@gmail.com>
2026-05-06 19:39:00 +0530

    fix homepage

--- app/page.js
+++ app/page.js
@@ -6,16 +6,5 @@ export default function Home() {
   redirect('/dashboard')
 }
 
-export default async function Home() {
-  const { data, error } = await supabase.from('clinics').select('*')
-
-  return (
-    <div>
-      <h1>Clinic App</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-      {error && <p>Error: {error.message}</p>}
-    </div>
-  )
-}
 
 console.log('pages loading')
